0f09717d6977121662fd96ab3188b9c65dd096c8
IGamesI <69199901+IGamesI@users.noreply.github.com>
2023-04-02 15:39:45 +0330

    Fixed Sidebar Active Class Bug

--- JavaScript/SidebarAjax.js
+++ JavaScript/SidebarAjax.js
@@ -1,9 +1,16 @@
-console.log("script added!");
+window.onload = function () {
+  window.location.href.indexOf("index.html");
+  if (window.location.href.indexOf("index.html") > -1) {
+    updateSidebar(window.location.href);
+    ("yep first time");
+    // Your function here
+  }
+};
 
 // Add click event listener to sidebar links
 document.querySelectorAll(".sidebar-link").forEach(function (link) {
   link.addEventListener("click", function (event) {
-    console.log("link clicked!");
+    ("link clicked!");
     event.preventDefault(); // Prevent default page navigation behavior
     var url = this.href; // Get link URL
 
@@ -15,7 +22,7 @@ document.querySelectorAll(".sidebar-link").forEach(function (link) {
 // Add click event listener to other links
 document.querySelectorAll(".link").forEach(function (link) {
   link.addEventListener("click", function (event) {
-    console.log("link clicked!");
+    ("link clicked!");
     event.preventDefault(); // Prevent default page navigation behavior
     var url = this.href; // Get link URL
     loadContent(url);
@@ -40,14 +47,14 @@ function loadContent(url) {
       tempDiv.innerHTML = xhr.responseText;
       // Get the page-content div's children
       var pageContentChildren = tempDiv.querySelector("#page-content").children;
-      console.log(pageContentChildren);
+      pageContentChildren;
       // Replace page content with loaded HTML data
       document.querySelector("#page-content").innerHTML = "";
       var forloops = pageContentChildren.length;
       for (var i = 0; i < forloops; i++) {
-        console.log(pageContentChildren);
-        console.log(pageContentChildren[0]);
-        console.log(i);
+        pageContentChildren;
+        pageContentChildren[0];
+        i;
         document
           .querySelector("#page-content")
           .appendChild(pageContentChildren[0]);
@@ -64,12 +71,14 @@ function loadContent(url) {
 
 // Update sidebar link highlighting based on current URL
 function updateSidebar(url) {
-  console.log("handling active class");
+  var filename = url.split("/").pop();
+  ("handling active class");
+  `href shall be ${url}`;
+  `href shall be ${filename}`;
   document.querySelectorAll(".sidebar-link").forEach(function (link) {
     link.classList.remove("active");
   });
   document
-    .querySelector(`.sidebar-link[href="${url}"]`)
-    .classList.add("active");
-  console.log(`href shall be ${url}`);
+    .querySelectorAll(`.sidebar-link[href*="${filename}"]`)
+    .forEach((link) => link.classList.add("active"));
 }
