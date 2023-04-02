5fe87f807e3e9923b937b2bb57b1161373745aa0
IGamesI <69199901+IGamesI@users.noreply.github.com>
2023-04-02 15:21:10 +0330

    Fixed page-content Bug

--- JavaScript/SidebarAjax.js
+++ JavaScript/SidebarAjax.js
@@ -8,6 +8,7 @@ document.querySelectorAll(".sidebar-link").forEach(function (link) {
     var url = this.href; // Get link URL
 
     loadContent(url); // Load new content
+    updateSidebar(url);
   });
 });
 
@@ -29,18 +30,32 @@ function removeSidebar() {
   }
 }
 
-// Load page content via AJAX
 function loadContent(url) {
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function () {
     if (xhr.readyState === 4 && xhr.status === 200) {
+      // Create a temporary div element
+      var tempDiv = document.createElement("div");
+      // Set the response text as innerHTML
+      tempDiv.innerHTML = xhr.responseText;
+      // Get the page-content div's children
+      var pageContentChildren = tempDiv.querySelector("#page-content").children;
+      console.log(pageContentChildren);
       // Replace page content with loaded HTML data
-      document.querySelector("#page-content").innerHTML = xhr.responseText;
+      document.querySelector("#page-content").innerHTML = "";
+      var forloops = pageContentChildren.length;
+      for (var i = 0; i < forloops; i++) {
+        console.log(pageContentChildren);
+        console.log(pageContentChildren[0]);
+        console.log(i);
+        document
+          .querySelector("#page-content")
+          .appendChild(pageContentChildren[0]);
+      }
       // Add page data to browser history
       history.pushState({}, "", url);
       // Update sidebar link highlighting
-      removeSidebar(); // Remove previous sidebar
-      updateSidebar(url);
+      // removeSidebar(); // Remove previous sidebar
     }
   };
   xhr.open("GET", url, true);
