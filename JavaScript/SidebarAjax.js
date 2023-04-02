37172d75645baf5ce75577f26c66a160869ff27e
IGamesI <69199901+IGamesI@users.noreply.github.com>
2023-04-02 15:00:00 +0330

    Fixed double sidebar bug

--- JavaScript/SidebarAjax.js
+++ JavaScript/SidebarAjax.js
@@ -1,5 +1,18 @@
+console.log("script added!");
+
 // Add click event listener to sidebar links
-document.querySelectorAll("sidebar-link").forEach(function (link) {
+document.querySelectorAll(".sidebar-link").forEach(function (link) {
+  link.addEventListener("click", function (event) {
+    console.log("link clicked!");
+    event.preventDefault(); // Prevent default page navigation behavior
+    var url = this.href; // Get link URL
+
+    loadContent(url); // Load new content
+  });
+});
+
+// Add click event listener to other links
+document.querySelectorAll(".link").forEach(function (link) {
   link.addEventListener("click", function (event) {
     console.log("link clicked!");
     event.preventDefault(); // Prevent default page navigation behavior
@@ -8,15 +21,13 @@ document.querySelectorAll("sidebar-link").forEach(function (link) {
   });
 });
 
-// Add click event listener to other links
-document.querySelectorAll("link").forEach(function (link) {
-  link.addEventListener("click", function (event) {
-    console.log("link clicked!");
-    event.preventDefault(); // Prevent default page navigation behavior
-    var url = this.href; // Get link URL
-    loadContent(url);
-  });
-});
+// Remove previous sidebar
+function removeSidebar() {
+  var sidebar = document.querySelector(".sidebar");
+  if (sidebar) {
+    sidebar.parentNode.removeChild(sidebar);
+  }
+}
 
 // Load page content via AJAX
 function loadContent(url) {
@@ -28,6 +39,7 @@ function loadContent(url) {
       // Add page data to browser history
       history.pushState({}, "", url);
       // Update sidebar link highlighting
+      removeSidebar(); // Remove previous sidebar
       updateSidebar(url);
     }
   };
@@ -38,9 +50,11 @@ function loadContent(url) {
 // Update sidebar link highlighting based on current URL
 function updateSidebar(url) {
   console.log("handling active class");
-  document.querySelectorAll("sidebar-link").forEach(function (link) {
+  document.querySelectorAll(".sidebar-link").forEach(function (link) {
     link.classList.remove("active");
   });
-  document.querySelector(`sidebar-link[href="${url}"]`).classList.add("active");
+  document
+    .querySelector(`.sidebar-link[href="${url}"]`)
+    .classList.add("active");
   console.log(`href shall be ${url}`);
 }
